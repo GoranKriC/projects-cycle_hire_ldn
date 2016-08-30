2a1a950bc875ea46f4761577790eff18059f28ac
datamaps <datamaps@homeserver>
2016-08-30 19:06:02 +0100

    first commit, ver 1.0

---
+++ find_postcodes.R
@@ -0,0 +1,84 @@
+lapply(c('data.table', 'DT', 'leaflet', 'RMySQL', 'shiny'), require, character.only = TRUE)
+db_conn = dbConnect(MySQL(), group = 'homeserver', dbname = 'londonCycleHire')
+
+strSQL <- "
+    SELECT * 
+    FROM stations 
+    WHERE OA_id = '0' AND lat > 0
+    ORDER BY postcode
+"
+stations <- data.table(dbGetQuery(db_conn, strSQL) )
+dbDisconnect(db_conn)
+
+ui <- fluidPage(
+        dataTableOutput('st_tbl'), hr(),
+        textInput('txtPostcode', 'POSTCODE:'),
+        fluidRow(
+            column(2, actionButton('btnUpdate', 'update station') ),
+            column(2, actionButton('btnUpdateOA', 'update OAs') )
+        ), hr(),
+        leafletOutput('st_map')
+)
+
+server <- function(input, output) {
+    
+    mp <- leaflet() %>% addProviderTiles("OpenStreetMap.BlackAndWhite")
+    
+    y <- stations[, .(station_id, id = paste('<a href="http://maps.google.com/maps?z=18&t=m&q=loc:', lat, ',', long, '" target="_blank">', station_id, '</a>', sep = ''), address, place, area, started = start_date, docks) ][order(area, place)]
+
+    output$st_tbl <- renderDataTable({
+        datatable(y,
+            rownames = FALSE,
+            selection = 'single',
+            class = 'cell-border stripe hover nowrap',
+            extensions = 'Scroller',
+            escape = FALSE,
+            options = list(
+                scrollX = TRUE,
+                scrollY = 300,
+                scroller = TRUE,
+                searchHighlight = TRUE,
+                columnDefs = list( list(targets = 0, visible = FALSE) ),
+                initComplete = JS(
+                    "function(settings, json) {",
+                    "$(this.api().table().header()).css({'background-color': '#238443', 'color': '#fff'});",
+                    "}"
+                ),
+                dom = 'frtip'
+            )
+        )
+    })
+    
+    selID <- reactive({
+        if(length(input$st_tbl_rows_selected ) == 0) return(NULL)
+        y[input$st_tbl_rows_selected, station_id]
+    })
+    
+    output$st_map <- renderLeaflet({
+        if(length(input$st_tbl_rows_selected ) == 0) return(NULL)
+        mp %>% 
+            setView(lng = stations[station_id == selID(), long], lat = stations[station_id == selID(), lat], zoom = 18) %>%
+            addMarkers(lng = stations[station_id == selID(), long], lat = stations[station_id == selID(), lat], popup = stations[station_id == selID(), address])
+    })
+    
+    observeEvent(
+        input$btnUpdate,
+        {
+            db_conn = dbConnect(MySQL(), group = 'homeserver', dbname = 'londonCycleHire')
+            dbSendQuery(db_conn, paste('UPDATE stations SET postcode = "', input$txtPostcode, '" WHERE station_id = ', selID(), sep = '') )
+            dbDisconnect(db_conn)
+        }
+    )
+    
+    observeEvent(
+        input$btnUpdateOA,
+        {
+            db_conn = dbConnect(MySQL(), group = 'homeserver', dbname = 'londonCycleHire')
+            dbSendQuery(db_conn, "UPDATE stations st JOIN geography.postcodes pc ON pc.postcode = st.postcode SET st.OA_id = pc.OA_id")
+            dbDisconnect(db_conn)
+        }
+    )
+    
+}
+
+shinyApp(ui = ui, server = server)
