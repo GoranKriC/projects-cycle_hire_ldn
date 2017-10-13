7209f99e11a12d3725248b68e2833bb6dacbd9e6
Luca <l.valnegri@datamaps.co.uk>
2017-10-13 11:40:02 +0100

    Create 91-data_display.R

---
+++ 91-data_display.R
@@ -0,0 +1,21 @@
+
+
+
+
+
+### SETUP ----------------------------------------------------------------------------------------------------------------------
+
+# load packages
+invisible(lapply(c('ggplot2', 'data.table', 'RMySQL', 'stringr'), require, character = TRUE))
+
+# set scheme
+scheme <- 1
+
+# Retrieve db name
+dbc = dbConnect(MySQL(), group = 'dataOps', dbname = 'common')
+db_name <- dbGetQuery(dbc, paste("SELECT db_name FROM cycle_hires WHERE scheme_id =", scheme))[[1]]
+dbDisconnect(dbc)
+
+# connect to database
+dbc = dbConnect(MySQL(), group = 'dataOps', dbname = db_name)
+
